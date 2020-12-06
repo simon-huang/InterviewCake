ae169631be4427df87eb9d19c3be271026efde3d
Simon Huang <simon.t.huang@gmail.com>
2020-12-05 22:26:08 -0800

    Fix formatting

--- ReverseStringInPlace.js
+++ ReverseStringInPlace.js
@@ -1,18 +1,18 @@
 function reverse(arrayOfChars) {
-// Reverse the input array of characters in place
-    var holder;
-    for (var i = 0; i < Math.floor(arrayOfChars.length/2); i++) {
-        holder = arrayOfChars[i];
-        arrayOfChars[i] = arrayOfChars[arrayOfChars.length-1-i];
-        arrayOfChars[arrayOfChars.length-1-i] = holder;
-    } 
-    return arrayOfChars;
-    //Complexity: O(n) time and O(1) space
+  // Reverse the input array of characters in place
+  var holder;
+  for (var i = 0; i < Math.floor(arrayOfChars.length / 2); i++) {
+    holder = arrayOfChars[i];
+    arrayOfChars[i] = arrayOfChars[arrayOfChars.length - 1 - i];
+    arrayOfChars[arrayOfChars.length - 1 - i] = holder;
+  }
+  return arrayOfChars;
+  //Complexity: O(n) time and O(1) space
 }
 
 /*
 //Solution
-  function reverse(arrayOfChars) {
+function reverse(arrayOfChars) {
 
   let leftIndex = 0;
   let rightIndex = arrayOfChars.length - 1;
@@ -29,4 +29,4 @@ function reverse(arrayOfChars) {
     rightIndex--;
   }
 }
-*/
+/*
