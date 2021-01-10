24d7bf16b7d4166c7a99f5afeec3f3b7e93e08e0
Simon Huang <simon.t.huang@gmail.com>
2021-01-10 01:03:56 -0800

    Implement getPath

--- meshMessage.js
+++ meshMessage.js
@@ -15,13 +15,72 @@ class Queue {
     }
 }
 
-function getPath(graph, startNode, endNode) {
-    // Find the shortest route in the network between the two users
-
-
-    return null;
+function reconstructPath(howWeReachedNodes, startNode, endNode) {
+    const reversedShortestPath = [];
+    let currentNode = endNode;
+    while (currentNode !== null) {
+        reversedShortestPath.push(currentNode);
+        currentNode = howWeReachedNodes[currentNode];
+    }
+    return reversedShortestPath.reverse();
 }
 
+function getPath(graph, startNode, endNode) {
+    if (!graph.hasOwnProperty(startNode)) {
+        throw new Error('Start node not in graph!');
+    }
+    if (!graph.hasOwnProperty(endNode)) {
+        throw new Error('End node not in graph!');
+    }
+
+    const nodesToVisit = new Queue();
+    nodesToVisit.enqueue(startNode);
+
+    const howWeReachedNodes = {};
+    howWeReachedNodes[startNode] = null;
+
+    while (nodesToVisit.size > 0) {
+        const currentNode = nodesToVisit.dequeue();
+        if (currentNode === endNode) {
+            return reconstructPath(howWeReachedNodes, startNode, endNode);
+        }
+        graph[currentNode].forEach(neighbor => {
+            if (!howWeReachedNodes.hasOwnProperty(neighbor)) {
+                nodesToVisit.enqueue(neighbor);
+                howWeReachedNodes[neighbor] = currentNode;
+            }
+        });
+    }
+    return null;
+}
+// O(N + M) time (all nodes + all edges)
+// O(N) space (the queue could contain all nodes)
+
+/*
+First try:
+Less efficient if I store each path in its own array
+
+function getPath(graph, startNode, endNode) {
+    // Find the shortest route in the network between the two users
+    if (!graph[startNode] || !graph[endNode]) {
+        throw new Error('User not in network');
+    }
+    let queue = [[startNode]];
+    while (queue.length) {
+        let path = queue.shift();
+        let lastUser = path[path.length - 1];
+        if (lastUser == endNode) {
+            return path;
+        }
+        for (let neighbor of graph[lastUser]) {
+            if (path.indexOf(neighbor) < 0) {
+                queue.push([...path, neighbor]);
+            }
+        }
+    }
+    return null;
+}
+*/
 
 // Tests
 const graph = {
