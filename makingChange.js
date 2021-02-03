46c5213ab3031143631e3da344c2220dbaf06182
Simon Huang <simon.t.huang@gmail.com>
2021-02-03 00:01:18 -0800

    Note an implementation using memoization

--- makingChange.js
+++ makingChange.js
@@ -32,7 +32,45 @@ function changePossibilities2(amountLeft, denominations) {
     helper(amountLeft, denominations, 0);
     return result;
 }
+// top-down with memoization
+class Change {
+    constructor() {
+        this.memo = {};
+    }
 
+    changePossibilitiesTopDown(amountLeft, denominations, currentIndex = 0) {
+        // Check our memo and short-circuit if we've already solved this one
+        const memoKey = [amountLeft, currentIndex].join(', ');
+        if (this.memo.hasOwnProperty(memoKey)) {
+            console.log('grabbing memo [' + memoKey + ']');
+            return this.memo[memoKey];
+        }
+        // Base cases:
+        // We hit the amount spot on. yes!
+        if (amountLeft === 0) return 1;
+
+        // We overshot the amount left (used too many coins)
+        if (amountLeft < 0) return 0;
+
+        // We're out of denominations
+        if (currentIndex === denominations.length) return 0;
+
+        console.log('checking ways to make ' + amountLeft + ' with [' + denominations.slice(currentIndex).join(', ') + ']');
+
+        // Choose a current coin
+        const currentCoin = denominations[currentIndex];
+        // See how many possibilities we can get
+        // for each number of times to use currentCoin
+        let numPossibilities = 0;
+        while (amountLeft >= 0) {
+            numPossibilities += this.changePossibilitiesTopDown(amountLeft, denominations, currentIndex + 1);
+            amountLeft -= currentCoin;
+        }
+        // Save the answer in our memo so we don't compute it again
+        this.memo[memoKey] = numPossibilities;
+        return numPossibilities;
+    }
+}
 // Tests
 let desc = 'sample input';
 let actual = changePossibilities(4, [1, 2, 3]);
